51814dcf741a41031ea34950cba9e7aaa82aa0c7
Aydan <aydanwhitton@gmail.com>
2023-02-11 23:51:12 +1100

    Added proper attributation and documentation

--- clipboard.c
+++ clipboard.c
@@ -1,4 +1,4 @@
-//code adapted from https://stackoverflow.com/questions/37295904/clipboard-and-xcb
+//code adapted from https://stackoverflow.com/questions/37295904/clipboard-and-xcb and https://github.com/jtanx/libclipboard
 //resource: https://www.x.org/releases/X11R7.6/doc/xorg-docs/specs/ICCCM/icccm.html#peer_to_peer_communication_by_means_of_selections
 
 #include <stdlib.h>
@@ -24,20 +24,21 @@ void clipboard_init(xcb_connection_t *connection, xcb_window_t window, char *lab
 	clipboard.connection = connection;
 	clipboard.window = window;
 	clipboard.source = NULL;
-	
-    xcb_intern_atom_cookie_t cookie_selection = xcb_intern_atom(clipboard.connection, 0, 9, "CLIPBOARD");
-    xcb_intern_atom_cookie_t cookie_property = xcb_intern_atom(clipboard.connection, 0, 9, label);
-    
-    xcb_intern_atom_reply_t* reply_selection  = xcb_intern_atom_reply(clipboard.connection, cookie_selection, NULL);
-    xcb_intern_atom_reply_t* reply_property   = xcb_intern_atom_reply(clipboard.connection, cookie_property, NULL);
-    
-    clipboard.selection = reply_selection->atom;
-    clipboard.property = reply_property->atom;
-    
-    free(reply_selection);
-    free(reply_property);
+
+	xcb_intern_atom_cookie_t cookie_selection = xcb_intern_atom(clipboard.connection, 0, 9, "CLIPBOARD");
+	xcb_intern_atom_cookie_t cookie_property = xcb_intern_atom(clipboard.connection, 0, 9, label);
+
+	xcb_intern_atom_reply_t* reply_selection  = xcb_intern_atom_reply(clipboard.connection, cookie_selection, NULL);
+	xcb_intern_atom_reply_t* reply_property   = xcb_intern_atom_reply(clipboard.connection, cookie_property, NULL);
+
+	clipboard.selection = reply_selection->atom;
+	clipboard.property = reply_property->atom;
+
+	free(reply_selection);
+	free(reply_property);
 }
 
+//clipboard_get: reads the clipboard from its current owner, copying it to the provided buffer
 size_t clipboard_get(char *str, size_t length) {
 	xcb_convert_selection(
 		clipboard.connection, clipboard.window, clipboard.selection, 
@@ -71,17 +72,20 @@ size_t clipboard_get(char *str, size_t length) {
 	return length;
 }
 
+//clipboard_set: takes ownership of the clipboard and caches the given string, which will later be provided to those requesting data from the clipboard
 void clipboard_set(char *str, size_t length) {
 	if (clipboard.source) free(clipboard.source);
 	clipboard.source = malloc(length);
 	clipboard.length = length;
 	memcpy(clipboard.source, str, clipboard.length);
-    xcb_set_selection_owner(clipboard.connection, clipboard.window, clipboard.selection, XCB_CURRENT_TIME);
+	xcb_set_selection_owner(clipboard.connection, clipboard.window, clipboard.selection, XCB_CURRENT_TIME);
 	xcb_flush(clipboard.connection);
 }
 
+//clipboard_selectionRequest: handles a selection request event and passes the clipboard data to the requestor, sends the selection notify event afterwards 
 void clipboard_selectionRequest(xcb_selection_request_event_t *event) {
-    if (event->property == XCB_NONE) event->property = event->target;
+	//todo: handle different targets correctly with proper timestamp support
+	if (event->property == XCB_NONE) event->property = event->target;
     
 	xcb_change_property(
 		clipboard.connection, XCB_PROP_MODE_REPLACE,
